add37c5114604a95180312b25d958d1e6990496f
zunpiau <1330456228@qq.com>
2018-03-04 00:11:53 +0800

    :recycle: Refactoring code.

--- src/main/java/io/github/zunpiau/config/DataConfig.java
+++ src/main/java/io/github/zunpiau/config/DataConfig.java
@@ -1,29 +1,27 @@
 package io.github.zunpiau.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.tomcat.jdbc.pool.DataSourceFactory;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @PropertySource({"classpath:jdbc-postgres.properties", "classpath:application.properties"})
 public class DataConfig {
 
-    @Value("${jdbc.password}")
-    private String password;
-
-    @Value("${jdbc.url}")
-    private String jdbcUrl;
-
-    @Value("${jdbc.username}")
-    private String username;
-
-    @Value("${jdbc.driver}")
-    private String driver;
+    @Bean(name = "jdbcProperties")
+    public PropertiesFactoryBean jdbcProperties() {
+        PropertiesFactoryBean bean = new PropertiesFactoryBean();
+        bean.setLocation(new ClassPathResource("jdbc-postgres.properties"));
+        return bean;
+    }
 
     @Bean
     public DataSourceTransactionManager txManager(DataSource dataSource) {
@@ -31,19 +29,8 @@ public class DataConfig {
     }
 
     @Bean
-    public DataSource dataSource() {
-        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
-        dataSource.setUrl(jdbcUrl);
-        dataSource.setDriverClassName(driver);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        dataSource.setInitialSize(4);
-        dataSource.setMinIdle(2);
-        dataSource.setMaxIdle(6);
-        dataSource.setTestOnBorrow(true);
-        dataSource.setRemoveAbandoned(true);
-        dataSource.setValidationQuery("SELECT 1");
-        return dataSource;
+    public DataSource dataSource(Properties jdbcProperties) throws Exception {
+        return new DataSourceFactory().createDataSource(jdbcProperties);
     }
 
     @Bean
